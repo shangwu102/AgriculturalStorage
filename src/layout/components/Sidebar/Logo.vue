c2e09b4a5c56b1e5ea351525aba1ae1423a07c89
wwcq <@030913>
2024-10-12 19:04:16 +0800

    修改标题

--- src/layout/components/Sidebar/Logo.vue
+++ src/layout/components/Sidebar/Logo.vue
@@ -21,7 +21,7 @@ export default {
   },
   data() {
     return {
-      title: '粮谋智链',
+      title: '藏粮于链',
       logo: 'https://wpimg.wallstcn.com/69a1c46c-eb1c-4b46-8bd4-e9e686ef5251.png'
     }
   }
